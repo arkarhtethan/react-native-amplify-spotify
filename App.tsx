0b8d422f33fe177f45f70db57d9c4dee38f6a1a3
Kaung Myat Han <arkarhtethan@gmail.com>
2022-04-15 03:17:33 +0630

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
 
-export default function App() {
+export default function App () {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
